407a509fb406bdf9c321b3403f2fbb6daf40e5b7
Genius1512 <silvan.genius@gmail.com>
2022-03-20 15:01:17 +0100

    Added docstrings to console.py

--- templates/console/console.py
+++ templates/console/console.py
@@ -5,24 +5,36 @@ from rich.status import Status
 class Console:
     @staticmethod
     def print(*objects):
+        """
+        Basic printing
+        """
         for object in objects:
             print(object, end=" ")
         print("")
     
     @staticmethod
     def success(*objects):
+        """
+        Success. Bold and green.
+        """
         for object in objects:
             print(f"[bold green]{object}[/bold green]", end=" ")
         print("")
     
     @staticmethod
     def warn(*objects):
+        """
+        Warning. Bold and yellow
+        """
         for object in objects:
             print(f"[bold yellow]{object}[/bold yellow]", end=" ")
         print("")
 
     @staticmethod
     def error(*objects):
+        """
+        Error. Bold and red
+        """
         for object in objects:
             print(f"[bold red]{object}[/bold red]", end=" ")
         print("")
@@ -30,6 +42,9 @@ class Console:
 
 class Spinner:
     def __init__(self, status: str, done_text: str = "Done."):
+        """
+        A basic status spinner
+        """
         self.done_text = done_text
         self.spinner = Status(
             status=status,
@@ -38,6 +53,9 @@ class Spinner:
         self.spinner.start()
 
     def stop(self):
+        """
+        Stop the spinner, show the done_text and delete self
+        """
         self.spinner.stop()
         print(self.done_text)
         del self
