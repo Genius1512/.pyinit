1ae9dd0c81b84647c50aa6a5a7cbf0f893066bc6
Genius1512 <silvan.genius@gmail.com>
2022-03-20 14:52:56 +0100

    Added console

--- templates/console/console.py
+++ templates/console/console.py
@@ -1,13 +1,54 @@
-# TODO: write script
-import sys
-
-from rich.console import Console as c
+from rich import print
+from rich.status import Status
 
 
 class Console:
     @staticmethod
-    def print(*objects, sep=" ", end="\n", file=sys.stdout, flush=False):
-        c.print(objects, sep=sep, end=end, file=file, flush=flush)
+    def print(*objects):
+        for object in objects:
+            print(object, end=" ")
+        print("")
+    
+    @staticmethod
+    def success(*objects):
+        for object in objects:
+            print(f"[bold green]{object}[/bold green]", end=" ")
+        print("")
+    
+    @staticmethod
+    def warn(*objects):
+        for object in objects:
+            print(f"[bold yellow]{object}[/bold yellow]", end=" ")
+        print("")
 
-    def error(*objects, sep=" ", end="\n", file=sys.stdout, flush=False):
-        c.print(objects, sep=sep, end=end, file=file, flush=flush, style="bold red")
+    @staticmethod
+    def error(*objects):
+        for object in objects:
+            print(f"[bold red]{object}[/bold red]", end=" ")
+        print("")
+
+
+class Spinner:
+    def __init__(self, status: str, done_text: str = "Done."):
+        self.done_text = done_text
+        self.spinner = Status(
+            status=status,
+            spinner="dots"
+        )
+        self.spinner.start()
+
+    def stop(self):
+        self.spinner.stop()
+        print(self.done_text)
+        del self
+
+
+if __name__ == "__main__":
+    Console.print("Basic")
+    Console.success("Success")
+    Console.warn("Warning")
+    Console.error("Error")
+
+    spinner = Spinner("Doing...", "Done")
+    __import__("time").sleep(2)
+    spinner.stop()
