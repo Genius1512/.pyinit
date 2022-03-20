6bf308cd171189cd7a50715546b47094840cc853
Genius1512 <79528697+Genius1512@users.noreply.github.com>
2022-03-20 13:39:47 +0000

    Started console class

--- templates/console/console.py
+++ templates/console/console.py
@@ -1,1 +1,13 @@
 # TODO: write script
+import sys
+
+from rich.console import Console as c
+
+
+class Console:
+    @staticmethod
+    def print(*objects, sep=" ", end="\n", file=sys.stdout, flush=False):
+        c.print(objects, sep=sep, end=end, file=file, flush=flush)
+
+    def error(*objects, sep=" ", end="\n", file=sys.stdout, flush=False):
+        c.print(objects, sep=sep, end=end, file=file, flush=flush, style="bold red")
